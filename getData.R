011cfefac1dc85674c9e3a78d6affc5523462c0c
Jun <majun0118@gmail.com>
2015-12-04 20:19:32 -0600

    add player name

--- getData.R
+++ getData.R
@@ -9,8 +9,9 @@ playerID <- player.info$PLAYER_ID
 shot.df <- data.frame()
 
 #get data for each player with a playerID
+ct <- 1
 for (playerID in player.info$PLAYER_ID[1:490]){
-  
+  Name <- player.info$PLAYER_NAME[ct]
   #URL from stat.nba.com for storing shooting data
   shotURL <- paste('http://stats.nba.com/stats/playerdashptshotlog?DateFrom=&DateTo=&GameSegment=&LastNGames=0&LeagueID=00&Location=&Month=0&OpponentTeamID=0&Outcome=&Period=0&PlayerID=', playerID, '&Season=2014-15&SeasonSegment=&SeasonType=Regular+Season&TeamID=0&VsConference=&VsDivision=', sep = '')
 
@@ -18,17 +19,20 @@ for (playerID in player.info$PLAYER_ID[1:490]){
   shotData <- fromJSON(txt = shotURL)
   
   #unlist and save
-  shot.df <- rbind(shot.df,data.frame(matrix(unlist(shotData$resultSets[3]), ncol=19, byrow = F)))
+  shot.df <- rbind(shot.df,data.frame(matrix(unlist(shotData$resultSets[3]), ncol=19, byrow = F), playerName = Name))
   
   #optional to check progress as it may take a while
   print(playerID)
+  ct <- ct +1
 }
 
 #colnames
-colnames(shot.df) <- unlist(shotData$resultSets[2])
+colnames(shot.df) <- c(unlist(shotData$resultSets[2]),'playerName')
 
 #out put .csv file
-write.csv(shot.df, file = 'shot.csv')
+#write.csv(shot.df, file = 'shot.csv')
 
 #check data
 head(shot.df)
+
+
